5eb1dbb68bbbed0a16fe3461a2a0a708bff5bd11
pomelo_hsieh <pomelo_hsieh@asus.com>
2019-06-13 12:38:25 +0800

    Add a install rknn-toolkit script.
    Change-Id: I917e272cba91c66b609eda3255a2e8982128b8c3
    Signed-off-by: pomelo_hsieh <pomelo_hsieh@asus.com>
    
    Former-commit-id: ddce6e7b33cbb7ca1da29b0f2785cc1107e04568

---
+++ overlay-debug/home/linaro/Desktop/Test_tool/rknn_toolkit/rknn_toolkit_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+pip3 install ./whls/*.whl --user
+sudo apt-get build-dep -y python3-h5py
+pip3 install h5py --user
+pip3 install tensorflow-1.11.0-cp35-none-linux_aarch64.whl --user
+pip3 install opencv_python_headless-4.0.1.23-cp35-cp35m-linux_aarch64.whl --user
+pip3 install rknn_toolkit-1.0.3b1-cp35-cp35m-linux_aarch64.whl --user
