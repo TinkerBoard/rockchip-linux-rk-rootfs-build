a68a54c3e9f6dacc7a104c4ecd3360b0371fd8d9
andy3_kuo <andy3_kuo@asus.com>
2019-06-24 11:00:23 +0800

    test tool: disable thermal policy at BurnIn test
    Change-Id: I0d8d903e13efb7ae46474c7fbb8fbdcd4ebcd3e1
    
    Former-commit-id: ecfbe07fcd5f5525d3078a1469b0d0b3c7d328ae

--- overlay-debug/home/linaro/Desktop/Test_tool/BurnIn_test/rockchip_test/high_performance.sh
+++ overlay-debug/home/linaro/Desktop/Test_tool/BurnIn_test/rockchip_test/high_performance.sh
@@ -5,16 +5,16 @@ echo "Setting CPU / GPU / DDR in highest performance mode"
 
 if [ -e /sys/class/thermal/thermal_zone0 ]; then
   echo user_space >/sys/class/thermal/thermal_zone0/policy
-#  echo disabled > /sys/class/thermal/thermal_zone0/mode
-#  echo 0 > /sys/class/thermal/thermal_zone0/cdev0/cur_state
-#  echo 0 > /sys/class/thermal/thermal_zone0/cdev1/cur_state
-#  echo 0 > /sys/class/thermal/thermal_zone0/cdev2/cur_state
+  echo disabled > /sys/class/thermal/thermal_zone0/mode
+  echo 0 > /sys/class/thermal/thermal_zone0/cdev0/cur_state
+  echo 0 > /sys/class/thermal/thermal_zone0/cdev1/cur_state
+  echo 0 > /sys/class/thermal/thermal_zone0/cdev2/cur_state
 
 fi
 
 if [ -e /sys/class/thermal/thermal_zone1 ]; then
   echo user_space > /sys/class/thermal/thermal_zone1/policy
-#  echo disabled > /sys/class/thermal/thermal_zone1/mode
+  echo disabled > /sys/class/thermal/thermal_zone1/mode
 fi
 
 echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
