6735c5202a445c95e69b8bfd3759104e4a7f05dd
andy3_kuo <andy3_kuo@asus.com>
2019-10-09 09:51:41 +0800

    test tool: remove debian version check at reboot & shutdown test
    Change-Id: Ic1cc16b572993fb69135af3ba141bd3e361d2606
    
    Former-commit-id: 6511606e19c3391ea3a11bfdb35007242b6d4261

--- overlay-debug/home/linaro/Desktop/Test_tool/System_test/etc/rc_reboot.sh
+++ overlay-debug/home/linaro/Desktop/Test_tool/System_test/etc/rc_reboot.sh
@@ -4,17 +4,7 @@ echo "Stress Test of Reboot."
 
 times=$(grep -r "reboot_times" /etc/reboot_times.txt | awk '{print $3}')
 
-cat /etc/debian_version | grep "9.9"
-if [ "$?" == "0" ]; then
-	echo "Debian Version 9.9."
-	echo "########## Reboot device from test_Reboot.sh ##########"
-	((times+=1))
-	echo "reboot_times = "$times > /etc/reboot_times.txt
-	systemctl reboot
-else
-	echo "Detect wrong debian version."
-	echo "########## Stop Reboot test ##########"
-	((times+=1))
-	echo "Detect wrong debian version." >> /etc/reboot_times.txt
-	echo "Reboot fail in the "$times" time." >> /etc/reboot_times.txt
-fi
+echo "########## Reboot device from test_Reboot.sh ##########"
+((times+=1))
+echo "reboot_times = "$times > /etc/reboot_times.txt
+systemctl reboot
--- overlay-debug/home/linaro/Desktop/Test_tool/System_test/etc/rc_shutdown.sh
+++ overlay-debug/home/linaro/Desktop/Test_tool/System_test/etc/rc_shutdown.sh
@@ -4,18 +4,8 @@ echo "Stress Test of Shutdown."
 
 times=$(grep -r "shutdown_times" /etc/shutdown_times.txt | awk '{print $3}')
 
-cat /etc/debian_version | grep "9.9"
-if [ "$?" == "0" ]; then
-	echo "Debian Version 9.9."
-	echo "########## Shutdown device from test_shutdown.sh ##########"
-	((times+=1))
-	echo "shutdown_times = "$times > /etc/shutdown_times.txt
-	echo +40 > /sys/class/rtc/rtc0/wakealarm
-	systemctl poweroff
-else
-	echo "Detect wrong debian version."
-	echo "########## Stop shutdown test ##########"
-	((times+=1))
-	echo "Detect wrong debian version." >> /etc/shutdown_times.txt
-	echo "Shutdown fail in the "$times" time." >> /etc/shutdown_times.txt
-fi
+echo "########## Shutdown device from test_shutdown.sh ##########"
+((times+=1))
+echo "shutdown_times = "$times > /etc/shutdown_times.txt
+echo +40 > /sys/class/rtc/rtc0/wakealarm
+systemctl poweroff
