df1f719c457b3a0e3a8d72d508ed14dbcde581ef
pomelo_hsieh <pomelo_hsieh@asus.com>
2020-08-27 10:09:20 +0800

    Add a setup rknn toolkit V1.4.0 script.
    Signed-off-by: pomelo_hsieh <pomelo_hsieh@asus.com>
    Change-Id: I425f55ebd80968d45165d0df6105de60fc501e30
    
    Former-commit-id: 66a3d6fea4086dd2224ad1cdbc472ed88c33fbb4

---
+++ overlay-debug/home/linaro/Desktop/Test_tool/npu_simple_test/local_install_rknn_toolkit_v1.4.0.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/linaro/Desktop/Test_tool/npu_simple_test/rknn_toolkit_v1.4.0_wheels
+sudo apt-get update
+sudo apt --fix-broken install -y
+sudo apt-get upgrade -y
+sudo apt-get install -y cmake gcc g++ libprotobuf-dev protobuf-compiler liblapack-dev libjpeg-dev zlib1g-dev python3-dev python3-pip python3-scipy python-pip libfreetype6-dev pkg-config libpng-dev
+sudo apt-get build-dep -y python3-h5py
+pip3 install --upgrade pip
+export PATH=/home/linaro/.local/bin:$PATH
+pip3 install enum34 futures --user
+pip3 install --no-index --find-links=/home/linaro/Desktop/Test_tool/npu_simple_test/rknn_toolkit_v1.4.0_wheels/ wheel setuptools h5py gluoncv --user
+pip3 install --no-index --find-links=/home/linaro/Desktop/Test_tool/npu_simple_test/rknn_toolkit_v1.4.0_wheels/ ./tensorflow-1.11.0-cp35-none-linux_aarch64.whl --user
+pip3 install --no-index --find-links=/home/linaro/Desktop/Test_tool/npu_simple_test/rknn_toolkit_v1.4.0_wheels/ ./opencv_python_headless-4.0.1.23-cp35-cp35m-linux_aarch64.whl --user
+pip3 install --no-index --find-links=/home/linaro/Desktop/Test_tool/npu_simple_test/rknn_toolkit_v1.4.0_wheels/ ./rknn_toolkit-1.4.0-cp35-cp35m-linux_aarch64.whl --user
---
+++ overlay-debug/home/linaro/Desktop/install-rknn-toolkit-v1.4.0.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function pause(){
+	read -n 1 -p "$*" INP
+	if [ $INP != '' ] ; then
+		echo -ne '\b \n'
+	fi
+}
+
+sudo apt-get update
+sudo apt-get install cmake gcc g++ libprotobuf-dev protobuf-compiler
+sudo apt-get install liblapack-dev libjpeg-dev zlib1g-dev
+sudo apt-get install python3-dev python3-pip python3-scipy
+pip3 install --upgrade pip
+pip3 install wheel setuptools
+sudo apt-get build-dep python3-h5py
+pip3 install h5py
+wget https://github.com/rockchip-linux/rknn-toolkit/releases/download/v1.4.0/rknn-toolkit-v1.4.0-packages.zip
+unzip ./rknn-toolkit-v1.4.0-packages.zip
+pip3 install ./packages/required-packages-for-arm64-debian9-python35/tensorflow-1.11.0-cp35-none-linux_aarch64.whl --user
+pip3 install ./packages/required-packages-for-arm64-debian9-python35/opencv_python_headless-4.0.1.23-cp35-cp35m-linux_aarch64.whl --user
+pip3 install ./packages/rknn_toolkit-1.4.0-cp35-cp35m-linux_aarch64.whl --user
+
+pause 'Press any key to continue...'
