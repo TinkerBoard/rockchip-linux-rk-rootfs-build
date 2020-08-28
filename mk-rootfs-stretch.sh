aaf6e716dcdd90f5d50796465b08dda3416aee7a
yupei_lin <yupei_lin@asus.com>
2020-08-28 17:15:52 +0800

    Audio : add shell script for switch alsa sink of pulse-audio
    usage : movesinks.sh <sink number or name>
    
    Change-Id: I477a9416085db8511272bd6fbbb2c7af1e4f04de
    
    Former-commit-id: 42495de99354d7b3749300a2b4ace9c98df389f6

--- mk-rootfs-stretch.sh
+++ mk-rootfs-stretch.sh
@@ -128,6 +128,9 @@ fi
 apt-get remove -y libgl1-mesa-dri:$ARCH xserver-xorg-input-evdev:$ARCH
 apt-get install -y libxfont1:$ARCH libinput-bin:$ARCH libinput10:$ARCH libwacom2:$ARCH libunwind8:$ARCH xserver-xorg-input-libinput:$ARCH libxml2-dev:$ARCH libglib2.0-dev:$ARCH libpango1.0-dev:$ARCH libimlib2-dev:$ARCH librsvg2-dev:$ARCH libxcursor-dev:$ARCH g++ make libdmx-dev:$ARCH libxcb-xv0-dev:$ARCH libxfont-dev:$ARCH libxkbfile-dev:$ARCH libpciaccess-dev:$ARCH mesa-common-dev:$ARCH libpixman-1-dev:$ARCH
 
+#--------------Audio--------------
+chmod 755 /etc/pulse/movesinks.sh
+
 #---------------Xserver--------------
 echo "deb http://http.debian.net/debian/ buster main contrib non-free" >> /etc/apt/sources.list
 apt-get update
