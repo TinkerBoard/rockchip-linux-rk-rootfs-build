455907cc28e0b298ccfcdd4a0beaed8ebf706043
Tzuhsuan_Chen <Tzuhsuan_Chen@asus.com>
2020-09-02 10:16:25 +0800

    leds: Support led control for emmc or sdcard read/write.
    Change-Id: I370f355bc265c472a37d5eb025a75861be30c906
    
    Former-commit-id: ce788acabcd3745f9d7ef9075a7bc04d3e0e6f5d

--- overlay/etc/init.d/rockchip.sh
+++ overlay/etc/init.d/rockchip.sh
@@ -144,6 +144,18 @@ then
     systemctl restart jack-switch-at-boot.service
 fi
 
+# set act-led trigger function
+cmdline=$(cat /proc/cmdline)
+storage=`echo $cmdline|awk '{print match($0,"storagemedia=emmc")}'`;
+
+if [ $storage -gt 0 ]; then
+    #emmc
+    echo mmc1 > /sys/class/leds/act-led/trigger
+else
+    #sdcard
+    echo mmc0 > /sys/class/leds/act-led/trigger
+fi
+
 # enable adbd service
 if [ -e "/etc/init.d/adbd.sh" ] ;
 then
