1fe4b9820d5355c41aa56d622cc4d0c7bbde85d6
andy3_kuo <andy3_kuo@asus.com>
2021-06-30 16:06:33 +0800

    test tool: change reboot / shutdown method
    Change-Id: Ie44ae6e12be7bebc45ffa0a947a2352902c150cc
    
    Former-commit-id: 787f5263d9fba039b9de63a61a4a4faebf054ccc

--- overlay-debug/home/linaro/Desktop/Test_tool/System_test/etc/rc_reboot.sh
+++ overlay-debug/home/linaro/Desktop/Test_tool/System_test/etc/rc_reboot.sh
@@ -7,4 +7,6 @@ times=$(grep -r "reboot_times" /etc/reboot_times.txt | awk '{print $3}')
 echo "########## Reboot device from test_Reboot.sh ##########"
 ((times+=1))
 echo "reboot_times = "$times > /etc/reboot_times.txt
-systemctl reboot
+#systemctl reboot
+echo 1 | sudo tee /proc/sys/kernel/sysrq
+echo b | sudo tee /proc/sysrq-trigger
--- overlay-debug/home/linaro/Desktop/Test_tool/System_test/etc/rc_shutdown.sh
+++ overlay-debug/home/linaro/Desktop/Test_tool/System_test/etc/rc_shutdown.sh
@@ -8,4 +8,6 @@ echo "########## Shutdown device from test_shutdown.sh ##########"
 ((times+=1))
 echo "shutdown_times = "$times > /etc/shutdown_times.txt
 echo +40 > /sys/class/rtc/rtc0/wakealarm
-systemctl poweroff
+#systemctl poweroff
+echo 1 | sudo tee /proc/sys/kernel/sysrq
+echo o | sudo tee /proc/sysrq-trigger
