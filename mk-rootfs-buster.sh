d2f5bb9966f4e1a52238c92b87b3df77c652a2a8
yupei_lin <yupei_lin@asus.com>
2021-09-28 16:34:58 +0800

    Audio : fix typo
    Change-Id: I58e7d91b8f1176c005c81ec4e798fb318725a912
    
    Former-commit-id: a76272344bc9cca95155b4d02d9ccf859e98566e

--- mk-rootfs-buster.sh
+++ mk-rootfs-buster.sh
@@ -242,7 +242,7 @@ cd /
 chmod 755 /etc/pulse/movesinks.sh
 chmod 755 /etc/audio/jack_auto_switch.sh
 chmod 755 /etc/audio/jack_switch_at_boot.sh
-ls -s /lib/systemd/system/jack-switch-at-boot.service /etc/systemd/system/multi-user.target.wants/jack-switch-at-boot.service
+ln -s /lib/systemd/system/jack-switch-at-boot.service /etc/systemd/system/multi-user.target.wants/jack-switch-at-boot.service
 chmod 755 /etc/audio/audio_setting.sh
 ln -s /lib/systemd/system/resume-onboard-audio.service /etc/systemd/system/suspend.target.wants/resume-onboard-audio.service
 chmod 755 /etc/audio/resume_onboard_audio.sh
