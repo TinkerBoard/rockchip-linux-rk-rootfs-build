8225dd5dc2eb58d72c19529e3dc798fe78a3b838
Tzuhsuan_Chen <Tzuhsuan_Chen@asus.com>
2022-06-22 17:31:26 +0800

    leds: Chnage the mmc number for EMMC/sdcard.
    Change-Id: I826c8a127a183dbbabaef8fc409c8e036491ba0e
    
    Former-commit-id: dfb88ab566bbcc8473435f1aacb8993465272e57

--- overlay/etc/init.d/rockchip.sh
+++ overlay/etc/init.d/rockchip.sh
@@ -118,10 +118,10 @@ storage=`echo $cmdline|awk '{print match($0,"storagemedia=emmc")}'`;
 
 if [ $storage -gt 0 ]; then
     #emmc
-    echo mmc1 > /sys/class/leds/act-led/trigger
+    echo mmc2 > /sys/class/leds/act-led/trigger
 else
     #sdcard
-    echo mmc0 > /sys/class/leds/act-led/trigger
+    echo mmc1 > /sys/class/leds/act-led/trigger
 fi
 
 # set cpu governor and frequence
