a27b8050d141b4fe39f5773b1994cd4a91c974ca
yupei_lin <yupei_lin@asus.com>
2022-06-27 17:24:59 +0800

    Audio : update sound card description for external sound card.
    Change-Id: I8afc247c38df991bb562d43533d0f6d1f7103bf2
    
    Former-commit-id: f7ec2f60f4158c94b49e095fb298bc1fe9359f20

--- overlay/etc/audio/jack_switch_at_boot.sh
+++ overlay/etc/audio/jack_switch_at_boot.sh
@@ -5,6 +5,10 @@ boardid=`cat /proc/boardid`
 echo project=${projectid};
 echo boardid=${boardid};
 
+sound_ext_card_name=`sudo -u linaro PULSE_RUNTIME_PATH=/run/user/1000/pulse pacmd list-cards | grep -A 10 alsa_card.platform-sound-ext-card | grep alsa.card_name`
+sound_ext_alsa_card_name=$(echo $sound_ext_card_name | cut -d" " -f 3)
+sudo -u linaro PULSE_RUNTIME_PATH=/run/user/1000/pulse pacmd update-sink-proplist alsa_output.platform-sound-ext-card.stereo-fallback device.description=$sound_ext_alsa_card_name
+
 if [ "${projectid}" == "7" ]; then
 	if [ "${boardid}" == "0" ]; then
 		exit 0
